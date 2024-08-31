3259a02a89dd7146ba2a24e400b8734e12066831
Keneth Fernando Vasquez Moran <aaldim@miumg.edu.gt>
2024-08-30 21:09:25 -0600

    primer commit

---
+++ application/models/Model_alumnos.php
@@ -0,0 +1,31 @@
+<?php
+class Model_alumnos extends CI_Model
+{
+    /**
+     * Constructor
+     */
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Obtiene los registros de alumnos
+     * @return [type] [arreglo de datos]
+     */
+
+
+
+    function getAlumnos()
+    {
+        $sql = "SELECT * FROM alumnos";
+        $query = $this->db->query($sql);
+        return $query->result();
+    }
+
+    function create($datos)
+	{
+		return $this->db->insert('alumnos', $datos);
+	}
+
+}
